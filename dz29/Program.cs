ddf9b10ab353001355038d672770eaf8c1c98d8a
Badri <9299547077@mail.ru>
2022-12-10 20:13:01 +0300

    Вывести массив из 8 элементов

---
+++ dz29/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] arr = Auto();
+System.Console.WriteLine("[" + string.Join(" | ", arr) + "]");
+
+int[] Auto()
+{
+    int[] arr = new int[8];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(5, 99);
+    }
+    return arr;
+}
